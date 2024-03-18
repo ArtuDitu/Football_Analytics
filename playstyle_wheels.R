1bdae1584292427cdaeda999c3639dcf009759b2
Artur Czeszumski <aczeszum@gmail.com>
2024-03-18 15:20:27 +0100

    add chance prevention calculation

--- playstyle_wheels.R
+++ playstyle_wheels.R
@@ -4,13 +4,38 @@ library(tidyverse)
 
 
 # Get the data for 7 leagues in season 2023/24
-d <- fb_season_team_stats(country = c("ITA", "ENG", "FRA", "GER", "ESP", "POR", "NED"), gender = "M", season_end_year = 2024, tier = "1st", stat_type = 'standard')
+d <- fb_season_team_stats(country = c("ITA", "ENG", "FRA", "GER", "ESP", "POR", "NED"), 
+                          gender = "M", 
+                          season_end_year = 2024, 
+                          tier = "1st", 
+                          stat_type = c('standard'))
+# get the possesion data
+d_possession <- fb_season_team_stats(country = c("ITA", "ENG", "FRA", "GER", "ESP", "POR", "NED"), 
+                                   gender = "M", 
+                                   season_end_year = 2024, 
+                                   tier = "1st", 
+                                   stat_type = c('possession'))
+
+# add new variable that adds middle and attacking third touches to the main data frame
+d$mid_att_touches <- d_possession$`Mid 3rd_Touches` + d_possession$`Att 3rd_Touches`
+
+
+
 # subset stats for each team
 d_for <- d %>%
   subset(Team_or_Opponent == 'team')
 # subset stats against each team
 d_against <- d %>%
   subset(Team_or_Opponent == 'opponent')
+
+
+### Defense
+## Chance prevention
+# Calculate the percentile ranks (inverted for reverse variable)
+ecdf_chance_prevention <- ecdf(d_against$npxG_Expected)  # Create the ECDF based on npxG
+d_against$chance_prevention <- sapply(d_against$npxG_Expected, function(x) (1 - ecdf_chance_prevention(x)) * 100) # Apply ECDF to each X value to get percentiles
+
+## Intensity
 
 
 
