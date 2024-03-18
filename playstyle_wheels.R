c9ca0a62a5e4a9e982a04c5d3b175ef5ea2c1122
Artur Czeszumski <aczeszum@gmail.com>
2024-03-18 15:31:30 +0100

    add intensity calculation

--- playstyle_wheels.R
+++ playstyle_wheels.R
@@ -9,7 +9,8 @@ d <- fb_season_team_stats(country = c("ITA", "ENG", "FRA", "GER", "ESP", "POR", 
                           season_end_year = 2024, 
                           tier = "1st", 
                           stat_type = c('standard'))
-# get the possesion data
+
+# get the possession data
 d_possession <- fb_season_team_stats(country = c("ITA", "ENG", "FRA", "GER", "ESP", "POR", "NED"), 
                                    gender = "M", 
                                    season_end_year = 2024, 
@@ -17,8 +18,7 @@ d_possession <- fb_season_team_stats(country = c("ITA", "ENG", "FRA", "GER", "ES
                                    stat_type = c('possession'))
 
 # add new variable that adds middle and attacking third touches to the main data frame
-d$mid_att_touches <- d_possession$`Mid 3rd_Touches` + d_possession$`Att 3rd_Touches`
-
+d$mid_att_touches_opponent <- d_possession$`Mid 3rd_Touches` + d_possession$`Att 3rd_Touches`
 
 
 # subset stats for each team
@@ -36,6 +36,8 @@ ecdf_chance_prevention <- ecdf(d_against$npxG_Expected)  # Create the ECDF based
 d_against$chance_prevention <- sapply(d_against$npxG_Expected, function(x) (1 - ecdf_chance_prevention(x)) * 100) # Apply ECDF to each X value to get percentiles
 
 ## Intensity
+ecdf_intensity <- ecdf(d_against$mid_att_touches_opponent)  # Create the ECDF based on npxG
+d_against$intensity <- sapply(d_against$mid_att_touches_opponent, function(x) (1 - ecdf_intensity(x)) * 100) # Apply ECDF to each X value to get percentiles
 
 
 
